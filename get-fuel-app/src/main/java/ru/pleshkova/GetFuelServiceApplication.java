ea429d8492f4fefbc76a6d97eb9385a652cdddf2
adpleshkova <anastasiiapleshkova@gmail.com>
2025-08-06 22:56:39 +0300

    init

---
+++ get-fuel-app/src/main/java/ru/pleshkova/GetFuelServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.pleshkova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetFuelServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetFuelServiceApplication.class, args);
+	}
+
+}
